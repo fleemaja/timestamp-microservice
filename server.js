468b7572f907a61fda8afe8dbf43773d02c4f12f
fleemaja <drew.fleeman@gmail.com>
2016-06-14 22:51:56 +0000

    timestamp microservice using express

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-'use strict';
-
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
+var moment = require('moment');
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+var path = process.cwd();
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.get("/", function(request, response) {
+  response.sendFile(path + '/index.html');
 });
+
+app.get("/:date", function(request, response) {
+  if (/^\d{8,}$/.test(request.params.date)) {
+    var myDate = moment(request.params.date, "X");
+  } else {
+    var myDate = moment(request.params.date, "MMMM D, YYYY");
+  }
+
+  if (myDate.isValid()) {
+    response.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    response.json({
+      unix: null,
+      natural: null
+    });
+  }
+});
+
+app.get("*", function(request, response) {
+  response.end("404!");
+});
+
+app.listen(8080);
